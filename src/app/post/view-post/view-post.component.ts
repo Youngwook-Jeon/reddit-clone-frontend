879ef74db1b5c065746c8abe7ff6501946035727
Young wook Jeon <ela87@naver.com>
2021-09-18 22:55:13 +0900

    Implement user profile page

--- src/app/post/view-post/view-post.component.ts
+++ src/app/post/view-post/view-post.component.ts
@@ -4,8 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { PostModel } from 'src/app/shared/post-model';
 import { throwError } from 'rxjs';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-// import { CommentPayload } from 'src/app/comment/comment.payload';
-// import { CommentService } from 'src/app/comment/comment.service';
+import { CommentPayload } from 'src/app/comment/comment.payload';
+import { CommentService } from 'src/app/comment/comment.service';
 
 @Component({
   selector: 'app-view-post',
@@ -17,37 +17,37 @@ export class ViewPostComponent implements OnInit {
   postId: number;
   post: PostModel;
   commentForm: FormGroup;
-  // commentPayload: CommentPayload;
-  // comments: CommentPayload[];
+  commentPayload: CommentPayload;
+  comments: CommentPayload[];
 
   constructor(private postService: PostService, private activateRoute: ActivatedRoute,
-    // private commentService: CommentService,
+    private commentService: CommentService,
     private router: Router) {
     this.postId = this.activateRoute.snapshot.params.id;
 
     this.commentForm = new FormGroup({
       text: new FormControl('', Validators.required)
     });
-    // this.commentPayload = {
-    //   text: '',
-    //   postId: this.postId
-    // };
+    this.commentPayload = {
+      text: '',
+      postId: this.postId
+    };
   }
 
   ngOnInit(): void {
     this.getPostById();
-    // this.getCommentsForPost();
+    this.getCommentsForPost();
   }
 
-  // postComment() {
-  //   this.commentPayload.text = this.commentForm.get('text').value;
-  //   this.commentService.postComment(this.commentPayload).subscribe(data => {
-  //     this.commentForm.get('text').setValue('');
-  //     this.getCommentsForPost();
-  //   }, error => {
-  //     throwError(error);
-  //   })
-  // }
+  postComment() {
+    this.commentPayload.text = this.commentForm.get('text').value;
+    this.commentService.postComment(this.commentPayload).subscribe(data => {
+      this.commentForm.get('text').setValue('');
+      this.getCommentsForPost();
+    }, error => {
+      throwError(error);
+    })
+  }
 
   private getPostById() {
     this.postService.getPost(this.postId).subscribe(data => {
@@ -57,12 +57,12 @@ export class ViewPostComponent implements OnInit {
     });
   }
 
-  // private getCommentsForPost() {
-  //   this.commentService.getAllCommentsForPost(this.postId).subscribe(data => {
-  //     this.comments = data;
-  //   }, error => {
-  //     throwError(error);
-  //   });
-  // }
+  private getCommentsForPost() {
+    this.commentService.getAllCommentsForPost(this.postId).subscribe(data => {
+      this.comments = data;
+    }, error => {
+      throwError(error);
+    });
+  }
 
 }
